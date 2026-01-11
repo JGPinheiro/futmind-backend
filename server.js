b8025db4e3486d17ed458b1c4d9d1357e2ac3ffe
JGPinheiro <jguiip45@gmail.com>
2026-01-11 17:59:23 -0300

    Create server.js

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from "express";
+import fetch from "node-fetch";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+
+const API_KEY = "SUA_CHAVE_AQUI"; // vamos trocar depois
+
+app.get("/live", async (req, res) => {
+  try {
+    const r = await fetch("https://v3.football.api-sports.io/fixtures?live=all", {
+      headers: { "x-apisports-key": API_KEY }
+    });
+    const data = await r.json();
+    res.json(data);
+  } catch (e) {
+    res.json({ error: "Erro na API" });
+  }
+});
+
+app.listen(3000, () => console.log("Rodando"));
